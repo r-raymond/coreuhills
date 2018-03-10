28e7cdaee0688019500664950f40972d819414cb
Robin Raymond <robin@robinraymond.de>
2018-03-10 11:53:01 +0100

    flesh out ls a little

--- ls/Main.hs
+++ ls/Main.hs
@@ -1,11 +1,24 @@
 module Main where
 
-import Protolude
+import           Protolude
 
-import Data.Text.IO (getContents)
+import           Data.Aeson       (encode)
+import           Data.List        (isPrefixOf)
+import           Data.Text.IO     (getContents)
+import           System.Directory (getDirectoryContents)
+
+import           Types
+
+hidden :: FilePath -> Bool
+hidden p = "." `isPrefixOf` p
 
 main :: IO ()
 main = do
-    input <- getContents
-    return getContents
+    --input <- getContents
+    --return getContents
+    dc <- getDirectoryContents "."
+    let d1 = filter (not . hidden) dc
+        d2 = sort d1
+    let paths = fmap (\p -> Path p Nothing Nothing Nothing Nothing Nothing) d2
+    putStrLn $ encode paths
 
